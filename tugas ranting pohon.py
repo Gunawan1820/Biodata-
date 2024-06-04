54ad5b66c468b62422dce3a92624abe4ba603f08
Gunawan <163821424+Gunawan1820@users.noreply.github.com>
2024-06-04 13:05:48 +0800

    Update tugas ranting pohon.py

--- tugas ranting pohon.py
+++ tugas ranting pohon.py
@@ -1,4 +1,4 @@
-#KELOMPOK 6
+²#KELOMPOK 6
 #1.LALU GUNAWAN JAYADI EFENDI
 #2.LALU ARIF RAHMAN HAKIM
 #3.MUHAMAD AMIZA SURYA
@@ -8,20 +8,15 @@ import turtle
 
 def draw_branch(branch_length, t):
     if branch_length > 5:
-        # Draw the branch
         t.forward(branch_length)
-        # Draw right subtree
         t.right(20)
         draw_branch(branch_length - 15, t)
-        # Draw left subtree
         t.left(40)
         draw_branch(branch_length - 15, t)
-        # Return to original orientation
         t.right(20)
         t.backward(branch_length)
 
 def draw_tree():
-    # Set up the turtle
     screen = turtle.Screen()
     screen.bgcolor("white")
     t = turtle.Turtle()
@@ -33,12 +28,9 @@ def draw_tree():
     t.down()
     t.speed(0)
 
-    # Draw the tree
     draw_branch(100, t)
 
-    # Finish up
     t.hideturtle()
     screen.mainloop()
 
-# Run the function
 draw_tree()
